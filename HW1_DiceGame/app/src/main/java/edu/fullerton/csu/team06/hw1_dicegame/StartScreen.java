fa094042ba9db523fc7ad12d7102dba8c98fd102
gitonga13 <gitonga13@users.noreply.github.com>
2018-09-26 08:40:40 -0700

    Added variables for player object

--- HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/StartScreen.java
+++ HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/StartScreen.java
@@ -5,7 +5,17 @@ import android.os.Bundle;
 import android.view.Window;
 
 public class StartScreen extends AppCompatActivity {
+    private int Dice1 = -1;
+    private int Dice2 = -1;
+    private int score = -1;
 
+    String buttonID = "name";
+
+    private boolean isAI = false;
+    private boolean turnCompleted = false;
+
+    //TODO: add array to check name. if name is buttonID changes for
+    // AI: Computer, 		USER:You
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
