3191c1844f00736f21975b6e66f6ac50652e59cb
Max Schlesinger <max.schlesinger@quanticmind.com>
2018-09-26 08:57:43 -0700

    Initial setup of Player object

---
+++ HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/Player.java
@@ -0,0 +1,14 @@
+package edu.fullerton.csu.team06.hw1_dicegame;
+
+public class Player {
+
+    protected int roll1;
+    protected int roll2;
+    protected int score;
+    protected String name;
+    protected Boolean isAI;
+    protected Boolean isTurnComplete;
+
+
+
+}
