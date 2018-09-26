01d8d0bf95f4a1b29c57c452de241dcb40dbefb2
asthrasher <ast.thrasher@gmail.com>
2018-09-26 08:18:08 -0500

    gameplay ui initial development
    4 dice that are actually just buttons. One of which is a spinner. No need for additional real estate for other buttons. referenced strings are used to display text on rolled buttons. I figured this be the easy way to change the value displayed on the button.

--- HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/StartScreen.java
+++ HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/StartScreen.java
@@ -1,14 +1,23 @@
 package edu.fullerton.csu.team06.hw1_dicegame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class StartScreen extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
     }
+
+    /** Called when the user taps the Play button */
+    public void playGame(View view) {
+        Intent intent = new Intent(this, Gameplay_UI.class);
+        startActivity(intent);
+    }
+
 }
