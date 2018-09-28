97487b7e34cfb2e104bda44ec9038e9748083200
MarshallGendron <marshallgendron@gmail.com>
2018-09-27 19:45:25 -0500

    updated function name to use correct standard. I didn't touch the functions Aaron created, thinking he might also be doing them.

--- HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/Gameplay_UI.java
+++ HW1_DiceGame/app/src/main/java/edu/fullerton/csu/team06/hw1_dicegame/Gameplay_UI.java
@@ -51,7 +51,7 @@ public class Gameplay_UI extends AppCompatActivity {
         Toast.makeText(getBaseContext(),"CPU rolls 2 dice",Toast.LENGTH_SHORT).show();
 
         // perform roll and get random number from 1-6 exclusive back
-        int roll_value = Perform_random_roll();
+        int roll_value = simulateSingleDiceRoll();
 
         // for now set button text to the roll value
         ((Button)view).setText(Integer.toString(roll_value));
@@ -69,14 +69,14 @@ public class Gameplay_UI extends AppCompatActivity {
         Toast.makeText(getBaseContext(),"Player rolls second dice",Toast.LENGTH_SHORT).show();
 
         // perform roll and get random number from 1-6 exclusive back
-        int roll_value = Perform_random_roll();
+        int roll_value = simulateSingleDiceRoll();
 
         // for now set button text to the roll value
         ((Button)view).setText(Integer.toString(roll_value));
     }
 
     /** Called to calculate/simulate a dice roll, returns a value of 1, 2, 3, 4, 5, or 6 */
-    private int Perform_random_roll(){
+    private int simulateSingleDiceRoll(){
         Random random = new Random();
         return random.nextInt(6) + 1;
     }
